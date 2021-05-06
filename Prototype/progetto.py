049aec54336548aa7efaa0377a62239404a699aa
Guillaume Quint <g.quint@studenti.unipi.it>
2021-05-06 15:05:21 +0200

    add zukunfty algorithm to logaritmicaly add new nodes in prototype py

--- Prototype/progetto.py
+++ Prototype/progetto.py
@@ -6,13 +6,13 @@ class Request:
     def __init__(self, _question, _asker):
         self.question = _question
         self.asker = _asker
-        self.answer = []
+        self.data = []
         self.answers = 0
 
 class DiscoveryServer:
     def __init__(self, numNodes):
         self.nei_chance = 2
-        self.nodes = {} # {(int)port : (Node)node}
+        self.nodes = [] # {(int)port : (Node)node}
         for i in range(numNodes):
             self.addNode()
 
@@ -20,21 +20,38 @@ class DiscoveryServer:
         if nodePort == -1:
             nodePort = randint(0, 9999)
         new_node = Node(nodePort)
-        done = len(self.nodes) < 1
-        while not done:
-            for p, n in self.nodes.items():
-                if randint(0, self.nei_chance - 1) == 0:
-                    done = True
-                    n.addNeighbours({nodePort : new_node})
-                    new_node.addNeighbours({p : n})
-        self.nodes[nodePort] = new_node
+    #   done = len(self.nodes) < 1
+    #   while not done:
+    #       for p, n in self.nodes.items():
+    #           if randint(0, self.nei_chance - 1) == 0:
+    #               done = True
+    #               n.addNeighbours({nodePort : new_node})
+    #               new_node.addNeighbours({p : n})
+    #   self.nodes[nodePort] = new_node
+
+        nid = len(self.nodes)
+        if nid == 0:
+            #print(nid)
+            #print("\n")
+            self.nodes.append(new_node)
+        else:
+            i = 1
+            #print(f"{nid} matches with:")
+            while nid % i == 0:
+                #print(nid-i)
+                new_node.addNeighbours({nid - i : self.nodes[nid - i]})
+                self.nodes[nid - i].addNeighbours({nid: new_node})
+                i *= 2
+            self.nodes.append(new_node)
+            #print("\n")
+
 
     def getNode(self, nodePort):
         return self.nodes[nodePort]
 
     def log(self):
         print(f"ho {len(self.nodes)} elementi:")
-        for _, n in self.nodes.items():
+        for _, n in enumerate(self.nodes):
             n.log()
 
 class Node:
@@ -87,17 +104,18 @@ class Node:
         print(f"ciao sono {self.port}\ti miei vicini sono {[p for p in self.neighbours.keys()]}\n\tpending requests: {self.pending_requests}\n\tactive_requests: {self.active_requests}")
 
 def main():
-    ds = DiscoveryServer(15)
-    ds.addNode(1337)
+    ds = DiscoveryServer(17)
     ds.log()
-    print("\n##########\n")
-
-    ds.getNode(1337).ask(42, Request("my question", -1))
-
-    while simulate:
-        for n in ds.nodes.values():
-            n.step()
-            n.log()
+#   ds.addNode(1337)
+#   ds.log()
+#   print("\n##########\n")
+#
+#   ds.getNode(1337).ask(42, Request("my question", -1))
+#
+#   while simulate:
+#       for n in ds.nodes.values():
+#           n.step()
+#           n.log()
 
 if __name__ == "__main__":
     main()
