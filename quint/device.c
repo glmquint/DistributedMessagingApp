5957ac0d7240068aeeba57735bd65da2dda6bf2c
Guillaume Quint <g.quint@studenti.unipi.it>
2022-03-03 19:10:47 +0100

    add parametric cmdlist to client

--- quint/device.c
+++ quint/device.c
@@ -19,11 +19,31 @@
 
 #define SCREEN_PRINT(x) printf("\r"); printf x; printf("\n>> "); fflush(stdout);
 
+struct Cmd_s {
+    char* name;
+    char* arguments[20];
+    int arg_num;
+    char* help;
+    bool requires_login;
+    bool always_print;
+};
+
+typedef struct Cmd_s Cmd;
+
 struct Device {
     bool is_logged_in;
     int port;
-    //char stdin_buffer[STDIN_BUF_LEN];
-} Device;
+    Cmd available_cmds[CMDLIST_LEN];
+} Device = {false, -1,{
+    {"signup", {"username", "password"}, 2, "crea un account sul server", false, false},
+    {"in", {"srv_port", "username", "password"}, 3, "richiede al server la connessione al servizio", false, false},
+    {"hanging", {""}, 0, "riceve la lista degli utenti che hanno inviato messaggi mentre si era offline", true, false},
+    {"show", {"username"}, 1, "riceve dal server i messaggi pendenti inviati da <username> mentre si era offline", true, false},
+    {"chat", {"username"}, 1, "avvia una chat con l'utente <username>", true, false},
+    {"share", {"file_name"}, 1, "invia il file <file_name> (nella current directory) al device su cui è connesso l'utente o gli utenti con cui si sta chattando", true, false},
+    {"out", {""}, 0, "richiede la disconnessione dal network", true, false},
+    {"esc", {""}, 0, "chiude l'applicazione", false, true},
+}};
 
 void Device_Init(int argv, char *argc[])
 {
@@ -37,7 +57,18 @@ void Device_Init(int argv, char *argc[])
 
 void Device_showMenu()
 {
-    SCREEN_PRINT(("\nDigita un comando:\n"));
+    SCREEN_PRINT(("        \nDigita un comando:\n"));
+
+    for (int i = 0; i < CMDLIST_LEN; i++) {
+        if (Device.available_cmds[i].always_print || Device.available_cmds[i].requires_login == Device.is_logged_in) {
+            char current_args[50] = "";
+            for (int j = 0; j < Device.available_cmds[i].arg_num; j++)
+                sprintf(current_args, "%s <%s>", current_args, Device.available_cmds[i].arguments[j]);
+            SCREEN_PRINT(("    %s%s --> %s", Device.available_cmds[i].name, current_args, Device.available_cmds[i].help));
+        }
+    }
+
+/*
     SCREEN_PRINT(("    signup <username> <password> --> crea un account sul server"));
     SCREEN_PRINT(("    in <srv_port> <username> <password> --> richiede al server la conneione al servizio"));
     SCREEN_PRINT(("    hanging --> riceve la lista degli utenti che hanno inviaato messaggi mentre si era offline"));
@@ -52,6 +83,7 @@ void Device_showMenu()
 void Device_esc()
 {
     printf("Arrivederci");
+    // SCREEN_PRINT(("Arrivederci!"));
     exit(0);
 }
 
