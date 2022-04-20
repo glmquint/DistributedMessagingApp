320ef5eddfc52d7732637031e889b4a660de3592
Guillaume Quint <g.quint@studenti.unipi.it>
2022-04-20 17:18:52 +0200

    fix cmd help bug

--- quint/util/cmd.c
+++ quint/util/cmd.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define SCREEN_PRINT(x) printf("\r"); printf x; printf("\n>> "); fflush(stdout);
 
@@ -21,6 +22,7 @@ void Cmd_showMenu(Cmd* cmd_list, int cmd_list_len, bool priv)
     SCREEN_PRINT(("        \nDigita un comando:\n"));
     for (i = 0; i < cmd_list_len; i++) {
         if (cmd_list[i].always_print || cmd_list[i].requires_login == priv) {
+            memset(current_args, '\0', sizeof(current_args));
             for (j = 0; j < cmd_list[i].arg_num; j++)
                 sprintf(current_args, "%s <%s>", current_args, cmd_list[i].arguments[j]);
             SCREEN_PRINT(("    %s%s --> %s", cmd_list[i].name, current_args, cmd_list[i].help_msg));
